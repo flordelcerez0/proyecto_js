af134c245417c7a8bc6b7a9d2539b74c2d1df5c4
florencia <flor.copertari@gmail.com>
2022-11-29 14:57:41 -0300

    fetch agregado carrito funcionando con sweetalert

--- script.js
+++ script.js
@@ -1,6 +1,4 @@
-
 let carritoGuardado = JSON.parse(localStorage.getItem('carrito')) || []
-
 let contenedorProductos = document.getElementById("contenedorProductos")
 
 fetch('./productos.json')
@@ -22,60 +20,32 @@ botonMacetas.onclick = () => {
 }
 
 function renderizarProductos(productosFiltrados) {
-    let productosARenderizar = productos
-    if (productosFiltrados) {
-        productosARenderizar = productosFiltrados
-    }
     contenedorProductos.innerHTML = ''
-    for (const {
-            imgUrl,
-            nombre,
-            precio,
-            stock,
-            id
-        } of productosARenderizar) {
+    productosFiltrados.forEach(producto => {
         let tarjetaProducto = document.createElement('article')
         tarjetaProducto.style.display = 'flex'
         tarjetaProducto.innerHTML = `
         <div class="card col-lg-6" style="width:500px" >
-            <img class="card-img-top img-fluid" src="${imgUrl}" alt="Card image">
+            <img class="card-img-top img-fluid" src="${producto.imgUrl}" alt="Card image">
             <div class="card-body">
-                <h4 class="card-title">${nombre}</h4>
-                <p class="card-text">$${precio}</p>
-                <p class="card-text">Quedan ${stock} unidades</p>
-                <a href="#" class="btn btn-primary" id=${id}>Agregar al carrito</a>
+                <h4 class="card-title">${producto.nombre}</h4>
+                <p class="card-text">$${producto.precio}</p>
+                <p class="card-text">Quedan ${producto.stock} unidades</p>
+                <a href="#" class="btn btn-primary" id=${producto.id}>Agregar al carrito</a>
             </div>
         </div>
         `
         contenedorProductos.append(tarjetaProducto)
-    }
+    })
 
     let botones = document.getElementsByClassName('btn btn-primary')
     let carrito = document.getElementById('carrito')
 
-    /*let carritoGuardado = []
-    if(localStorage.getItem('carrito')){
-        carritoGuardado = JSON.parse(localStorage.getItem('carrito'))
-    }*/
-
-
-
-    /*for (const item of carritoGuardado) {
-        let productoBuscado = productos.find(producto => producto.id == item.id)
-        carrito.innerHTML += `
-        <p>${productoBuscado.nombre}</p>
-        <p>$${productoBuscado.precio}</p>
-        `
-    }*/
 
 
     for (const boton of botones) {
         boton.onclick = (e) => {
-            let productoBuscado = productos.find(producto => producto.id == e.target.id)
-            /*carrito.innerHTML += `
-        <p>${productoBuscado.nombre}</p>
-        <p>$${productoBuscado.precio}</p>
-        `*/
+            let productoBuscado = productosFiltrados.find(producto => producto.id == e.target.id)
             carritoGuardado.push({
                 id: productoBuscado.id,
                 nombre: productoBuscado.nombre,
@@ -90,25 +60,25 @@ function renderizarProductos(productosFiltrados) {
             localStorage.setItem('carrito', JSON.stringify(carritoGuardado))
         }
     }
-}
 
-const verCarrito = document.getElementById("verCarrito")
-verCarrito.onclick = () => {
-    for (const item of carritoGuardado) {
-        let productoBuscado = productos.find(producto => producto.id == item.id)
-        //carrito.classList.toggle("oculto")
-        const Toast = Swal.mixin({
-            toast: true,
-            position: 'top-end',
-            showConfirmButton: false,
-            didOpen: (toast) => {
-                toast.addEventListener('mouseenter', Swal.stopTimer)
-                toast.addEventListener('mouseleave', Swal.resumeTimer)
-            }
-        })
-        Toast.fire({
-            title: 'Carrito',
-            text: `${productoBuscado.nombre} $${productoBuscado.precio}`
-        })
+
+    const verCarrito = document.getElementById("verCarrito")
+    verCarrito.onclick = () => {
+        for (const item of carritoGuardado) {
+            let productoBuscado = productosFiltrados.find(producto => producto.id == item.id)
+            const Toast = Swal.mixin({
+                toast: true,
+                position: 'top-end',
+                showConfirmButton: false,
+                didOpen: (toast) => {
+                    toast.addEventListener('mouseenter', Swal.stopTimer)
+                    toast.addEventListener('mouseleave', Swal.resumeTimer)
+                }
+            })
+            Toast.fire({
+                title: 'Carrito',
+                text: `${productoBuscado.nombre} $${productoBuscado.precio}`
+            })
+        }
     }
 }
